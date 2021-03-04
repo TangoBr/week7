e853b0612bfb1c3d716ac534e9ec500d525b666f
TangoBr <thomasbridges01@gmail.com>
2021-03-03 22:11:23 -0500

    js

---
+++ w7.js
@@ -0,0 +1,32 @@
+let spring = "best";
+console.log(spring);
+
+spring = "the better fall";
+console.log(spring);
+
+const why = [`fresh`, ` spring green leaves`, `flowers`, `sun`];
+
+if (true) {
+    var x = "whateverYouWant";
+} ;
+console.log(x);
+
+if (true) {
+    let samurai = `badass`;
+    console.log(samurai);
+} ;
+// console.log(samurai); if you wanted an error
+
+// why = "error bro"
+// console.log(why)
+
+const empty = [];
+empty.push("stuff", "other stuff", "third thing");
+console.log(empty);
+
+
+var me = {
+    name: "Thomas",
+    age : 24,
+};
+console.log(me)
