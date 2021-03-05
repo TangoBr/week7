b1b6908824a6f68232a02c51baaee92f761159b9
TangoBr <thomasbridges01@gmail.com>
2021-03-04 20:12:04 -0500

    stuff

--- hw1.js
+++ hw1.js
@@ -29,4 +29,31 @@ var me = {
     name: "Thomas",
     age : 24,
 };
-console.log(me)
+console.log(me);
+
+// template literal
+
+const hey =  (`hello I am ${me.name}`);
+console.log(hey);
+
+function greet (){
+    console.log(`Hello I am ${me.name} and I am ${me.age} years old.`)
+}; 
+greet();
+
+const mi = {
+    name : "Thomas",
+    age: 24 
+};
+console.log(mi.name);
+console.log(mi.age);
+
+const arrow1 = () => {
+    console.log(`hello world`)
+};
+arrow1 ();
+
+const arrow2 = (str) => {
+    console.log(`any parameter`)
+}
+arrow2 ();
