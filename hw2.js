3ec66ccc1170be8919897027f0158571ce437c3f
TangoBr <thomasbridges01@gmail.com>
2021-03-09 21:42:50 -0500

    oop

---
+++ hw2.js
@@ -0,0 +1,77 @@
+class Person  {
+    constructor (name, age){
+        this.name = name
+        this.age = age 
+    }
+        getName (){
+            return(this.name)
+        };
+};
+
+class Employee extends Person {
+    constructor(name, age, employeeID){
+        super(name, age);
+        this.employeeID = employeeID 
+        }
+    static getCompanyName () {
+        console.log(`Web development solutions incorporated`)
+    }
+    getEmployeeID () {
+        return(this.employeeID)
+    }
+};
+
+class Manager extends Employee {
+    constructor(name, age, employeeID, employees){
+        super(name, age, employeeID);
+        this.employees = employees
+    }
+    newEmployee (employeeID, employeeName) {
+        this.employees.push({employeeID, employeeName});
+    }
+    getEmployees () {
+        return (this.employees);
+    }
+};
+
+
+class Clerk extends Employee {
+    constructor(name, age, employeeID, supervisorID){
+    super(name, age, employeeID)
+    this.supervisorID = supervisorID 
+    }
+    getSupervisor () {
+        return (this.supervisorID);
+    }
+};
+
+let currentID = 1;
+
+const boss = new Manager (
+    `carl`,
+    34,
+    currentID++,
+    []
+);
+
+const clerk1 = new Clerk (
+    `Rosa`,
+    25,
+    currentID++,
+    boss.getEmployeeID(),
+);
+
+const clerk2 = new Clerk (
+    `karen`,
+    43,
+    currentID++,
+    boss.getEmployeeID(),
+);
+
+boss.newEmployee(clerk1.getEmployeeID(), clerk1.getName());
+boss.newEmployee(clerk2.getEmployeeID(), clerk2.getName());
+
+console.log(clerk1.getSupervisor());
+console.log(clerk2.getSupervisor());
+
+console.log(boss.getEmployees());
